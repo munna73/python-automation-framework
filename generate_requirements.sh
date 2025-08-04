a67af19caa14b7b7f3e647e075a8a3b5a39462c1
munna73 <karimmohammed@Karims-MacBook-Air.local>
2025-08-04 09:45:39 -0400

    Remove folder from repository

--- generate_requirements.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Activate existing virtual environment
-source venv/bin/activate
-
-# Freeze current environment into requirements.txt
-pip freeze > requirements.txt
-
-echo "📄 requirements.txt updated with current packages"
